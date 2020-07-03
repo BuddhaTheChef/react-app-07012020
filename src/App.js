1f37b90f8903b0530e4a09f0947131ad071fc4d9
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-07-03 15:34:54 -0700

    added in more routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Link, Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Media from "./components/Media/Media";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -11,10 +13,14 @@ function App() {
       <div>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/media">Media</Link>
+        <Link to="/contact">Contact</Link>
       </div>
       <Switch>
         <Route component={Home} exact path="/" />
         <Route component={About} path="/about" />
+        <Route component={Media} path="/media" />
+        <Route component={Contact} path="/contact" />
       </Switch>
     </div>
   );
