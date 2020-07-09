85a7843310c824faa2950cdfc70258c71ffe2471
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-07-08 17:01:04 -0700

    added in two new routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Media from "./components/Media/Media";
 import Contact from "./components/Contact/Contact";
+import Gallery1 from "./components/Gallery1/Gallery1";
+import Gallery2 from "./components/Gallery2/Gallery2";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
     <ul id="menu">
     <Link to="/" className="nav-item">Home</Link>
         <Link to="/about" className="nav-item">About</Link>
+        <Link to="/gallery-one" className="nav-item">Gallery1</Link>
+        <Link to="/gallery-two" className="nav-item">Gallery2</Link>
         <Link to="/media" className="nav-item">Media</Link>
         <Link to="/contact" className="nav-item">Contact</Link>
     </ul>
@@ -30,6 +34,8 @@ function App() {
       <Switch>
         <Route component={Home} exact path="/" />
         <Route component={About} path="/about" />
+        <Route component={Gallery1} path="/gallery-one" />
+        <Route component={Gallery2} path="/gallery-two" />
         <Route component={Media} path="/media" />
         <Route component={Contact} path="/contact" />
       </Switch>
