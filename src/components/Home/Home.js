c20a2256462d3dc3397f537305bf5350678b0cdc
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-07-09 17:59:13 -0700

    added in video

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Video1 from "../../assets/sewing-vid.mp4"
 import logo from "../../logo.svg";
 import "../../App.css";
 
@@ -7,6 +7,20 @@ export default class Home extends Component {
   render() {
     return (
       <div className="App">
+          <div id="wrap_video">
+
+<div id="video_box">
+    <div id="video_overlays">
+        <div>
+            <h1>HELLO</h1>
+        </div>
+    </div>
+    <div>
+        <video id="player" autoPlay muted loop src={Video1} type="video/mp4">Your browser does not support this streaming content.</video>
+    </div>
+</div>
+    
+</div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Yo Duder!</h1>
