c818af8c75e15c5d19e958f3e4f5343517a693a6
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-07-12 15:14:18 -0700

    added in stuff

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,95 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { DetailsList} from "@fluentui/react";
+
+import dtsaccounts from "../../dtsaccounts.json";
+
+const Columns = [
+  {
+    key: 1,
+    name: "ID",
+    fieldName: "name",
+    minWidth: 100,
+    maxWidth: 200,
+    isResizable: true,
+  },
+  {
+    key: 2,
+    name: "Account Name",
+    fieldName: "name",
+    minWidth: 100,
+    maxWidth: 200,
+    isResizable: true,
+  },
+  {
+    key: 3,
+    name: "Account Owner",
+    fieldName: "value",
+    minWidth: 100,
+    maxWidth: 200,
+    isResizable: true,
+  },
+  {
+    key: "column4",
+    name: "Primary Transfer Scenario",
+    fieldName: "value",
+    minWidth: 100,
+    maxWidth: 200,
+    isResizable: true,
+  },
+  {
+    key: "column5",
+    name: "Amount Billed Last Month",
+    fieldName: "value",
+    minWidth: 100,
+    maxWidth: 200,
+    isResizable: true,
+  },
+  {
+    key: "column6",
+    name: "Shares Information",
+    fieldName: "value",
+    minWidth: 100,
+    maxWidth: 200,
+    isResizable: true,
+  },
+];
+
+
 
 export default class About extends Component {
-    render() {
-        return (
+    constructor(props) {
+        super(props);
+
+        this.dtsaccounts = [];
+        for (let i = 0; i < dtsaccounts.length -1; i++) {
+          this.dtsaccounts.push({
+            key: dtsaccounts.id,
+            name: 'Item ' + dtsaccounts.name,
+            value: i,
+          });
+        }
+
+        this.state = {
+            items: this.dtsaccounts,
+          };
+      }
+  render() {
+    const { items } = this.state;
+
+    return (
+      <div>
+        <h1>About</h1>
+        
+        <DetailsList compact columns={Columns} items={items} />
+
+        {/* {dtsaccounts.map((dtsaccounts) => {
+          return (
             <div>
-                <h1>About</h1>
+              <p>{dtsaccounts.id}</p>
             </div>
-        )
-    }
+          );
+        })} */}
+      </div>
+    );
+  }
 }
