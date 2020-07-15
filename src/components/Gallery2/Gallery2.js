6f499c6825b06b180a53e4992fcf6cf4d1a5d29a
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-07-15 12:46:16 -0700

    added in template for gallery 2 layout

--- src/components/Gallery2/Gallery2.js
+++ src/components/Gallery2/Gallery2.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react'
+import './Gallery.css'
 
 export default class Gallery2 extends Component {
     render() {
         return (
             <div>
-                <h1 className="main-header">Gallery 2</h1>
+            <h1 className="header">Gallery 2</h1>
+            <div className="main-div">
+  
+              <div className="top-row">
+              <div className="content">
+                  1
+              </div>
+              <div className="content">
+                  2
+              </div>
+              <div className="content">
+                  3
+              </div>
             </div>
+  
+            <div className="second-row">
+              <div className="content">
+                  4
+              </div>
+              <div className="content">
+                  5
+              </div>
+              <div className="content">
+                  6
+              </div>
+            </div>
+  
+            <div className="third-row">
+              <div className="content">
+                  7
+              </div>
+              <div className="content">
+                  8
+              </div>
+              <div className="content">
+                  9
+              </div>
+            </div>
+  
+            </div>
+        </div>
         )
     }
 }
