5da3866899c5339a15e8dea3d7cac753b90bf8b4
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-07-29 12:46:05 -0700

    added in template

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -9,26 +9,26 @@ export default class Media extends Component {
                 <h1 className="main-header">Media</h1>
           <div className="main-div">
 
-            <div className="top-row">
-            <div className="content">
+            <div className="top-row-m">
+            <div className="content-m">
                 1
             </div>
-            <div className="content">
+            <div className="content-m">
                 2
             </div>
-            <div className="content">
+            <div className="content-m">
                 3
             </div>
           </div>
 
-          <div className="top-row">
-            <div className="content">
+          <div className="top-row-m">
+            <div className="content-m">
                 1
             </div>
-            <div className="content">
+            <div className="content-m">
                 2
             </div>
-            <div className="content">
+            <div className="content-m">
                 3
             </div>
           </div>
@@ -36,14 +36,14 @@ export default class Media extends Component {
 
    
 
-            <div className="top-row">
-            <div className="content">
+            <div className="top-row-m">
+            <div className="content-m">
                 7
             </div>
-            <div className="content">
+            <div className="content-m">
                 8
             </div>
-            <div className="content">
+            <div className="content-m">
                 9
             </div>
           </div>
