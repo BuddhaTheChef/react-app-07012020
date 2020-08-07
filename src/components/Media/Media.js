9d9c25a169c469d657ce3e08c96a161f019b6a6c
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-08-07 16:18:05 -0700

    adding in details for media page

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Footer from '../Footer/Footer'
 import '../Gallery1/Gallery.css'
+import Pic1 from "../../assets/newTech2.jpg"
 
 export default class Media extends Component {
     render() {
@@ -8,10 +9,9 @@ export default class Media extends Component {
             <div>
                 <h1 className="main-header">Media</h1>
           <div className="main-div">
-
+            
             <div className="top-row-m">
-            <div className="content-m">
-                Instagram Icon
+            <div id="content1" className="content-m">
             </div>
             <div className="content-m">
                 Fb
