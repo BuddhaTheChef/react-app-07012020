36b473194267f5f9d4c4ff199255683ae8ff3e1c
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-08-11 16:55:21 -0700

    added in correct layout

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,57 +1,39 @@
-import React, { Component } from 'react'
-import Footer from '../Footer/Footer'
-import '../Gallery1/Gallery.css'
-import Pic1 from "../../assets/newTech2.jpg"
+import React, { Component } from "react";
+import Footer from "../Footer/Footer";
+import "../Gallery1/Gallery.css";
+import Pic1 from "../../assets/newTech2.jpg";
 
 export default class Media extends Component {
     render() {
         return (
             <div>
                 <h1 className="main-header">Media</h1>
-          <div className="main-div">
-            
-            <div className="top-row-m">
-            <div id="content1" className="content-m">
-            </div>
-            <div className="content-m">
-            <div id="content1" className="content-m">
-            </div>
-            <div className="content-m">
-            <div id="content1" className="content-m"></div>
-            </div>
-          </div>
+                <div className="main-div">
+                    <div className="top-row-m">
+                        <div id="content1" className="content-m"></div>
 
-          <div className="top-row-m">
-            <div className="content-m">
-            <div id="content1" className="content-m"></div>
-            </div>
-            <div className="content-m">
-            <div id="content1" className="content-m"></div>
-            </div>
-            <div className="content-m">
-            <div id="content1" className="content-m"></div>
-            </div>
-          </div>
+                        <div id="content1" className="content-m"></div>
 
+                        <div id="content1" className="content-m"></div>
+                    </div>
+                    <div className="top-row-m">
+                        <div id="content1" className="content-m"></div>
 
-   
+                        <div id="content1" className="content-m"></div>
 
-            <div className="top-row-m">
-            <div className="content-m">
-                7
-            </div>
-            <div className="content-m">
-                8
-            </div>
-            <div className="content-m">
-                9
-            </div>
-          </div>
+                        <div id="content1" className="content-m"></div>
+                    </div>
+                    <div className="top-row-m">
+                        <div id="content1" className="content-m"></div>
 
-          </div>
-        
-                <Footer/>
+                        <div id="content1" className="content-m"></div>
+
+                        <div id="content1" className="content-m"></div>
+                    </div>
+                </div>
+
+                <Footer />
             </div>
-        )
+        );
     }
 }
